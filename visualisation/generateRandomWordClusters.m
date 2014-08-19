8b76a0eec56bcf93cf6639fed06d5fdfc0e24223
owenj <owenj@UK-OWENJ.dhcp.mathworks.com>
2014-08-19 17:51:31 +0100

    Added a few initial visualisation functions to experiment with.

---
+++ visualisation/generateRandomWordClusters.m
@@ -0,0 +1,46 @@
+function [words, wordCount, corrMat, tree] = generateRandomWordClusters(varargin)
+    
+    %% generate random list using an existing wordlist....
+    words = {'apple', 'apricot', 'avocado', 'banana', 'berry', 'blackberry', ...
+        'blood orange', 'blueberry', 'cantaloupe', 'cherry', 'citron', 'citrus', ...
+        'coconut','crabapple', 'cranberry', 'currant', 'date', 'dragonfruit', ...
+        'durian', 'elderberry', 'fig', 'grape', 'grapefruit', 'guava', 'honeydew', ...
+        'kiwi', 'kumquat', 'lemon', 'lime', 'lingonberry', 'loquat', 'lychee', ...
+        'mandarin orange', 'mango', 'melon', 'nectarine', 'orange', 'papaya', ...
+        'passion fruit', 'peach', 'pear', 'persimmon', 'pineapple', 'plantain', ...
+        'plum', 'pomegranite', 'prune', 'quince', 'raisin', 'raspberry', ...
+        'star fruit', 'strawberry', 'tangelo', 'tangerine', 'watermelon'};
+    % pick N or 30 random words
+    if nargin == 0
+        nwords = 30;
+    else
+        nwords = varargin{1};
+    end
+    
+    words = words(randperm(nwords));
+    
+    %% generate random word counts
+    wordCountStd = 25;
+    wordCountMean = 10;
+    nwords = numel(words);
+    wordCount = round(abs(rand([1,nwords])*wordCountStd + wordCountMean));
+    
+    %% generate correlationMatrix values between -1 and 1
+    % how many independent distances are there between nwords points?
+    ndistances = sum(1:(nwords-1));
+    % generate some random distances (as a vector) for the linkage function
+    distances = (rand(1,ndistances).*2)-1;
+    % the correlation matrix is a nice square version of these distances
+    % where corrMat(word1, word2) gives the correlation between any 2 words
+    corrMat = squareform(distances);
+    
+    %% generate clustering
+    % linkage does not like negative numbers and 0 indicates closeness and
+    % 1 difference, so this needs rescaling
+    rescaledDistances = (-0.5*distances+0.5);
+    tree = linkage(rescaledDistances);
+    bestOrder = optimalleaforder(tree, rescaledDistances);
+    hdendro = dendrogram(tree, 'Reorder', bestOrder);
+    set(gca, 'XTickLabel', words(bestOrder), 'XTickLabelRotation',90);
+    
+end
---
+++ visualisation/generateTextHandles.m
@@ -0,0 +1,97 @@
+function textGroup = generateTextHandles(words, wordCount, corrMat, tree)
+    %GENERATETEXTHANDLES Generates nicely coloured and sized text handles
+    %for use with the visualisation
+    % WORDS a cell array containing N text strings to turn into handles.
+    % WORDCOUNTS the number of times each word was counted, so it can be
+    % resized appropriately
+    % CORRMAT a N x N matrix with data ranged -1 (fully anti correlated)
+    % to 1 (fully correlated) describing how correlated each word is with
+    % each other word.
+    
+    
+    
+    % automatically calculate the best order for the words to go in for best
+    % clustering
+    % todo: THIS IS PROBABLY NOT THE BEST ORDERING ALGORITHM.
+    %bestOrder = optimalleaforder(tree, squareform(corrMat)+1);
+    f = figure;
+    [~,~,bestOrder] = dendrogram(tree);
+    close(f);
+    
+    colours = generateWordRGBColours(bestOrder, corrMat);
+    r = colours(:,1)';
+    g = colours(:,2)';
+    b = colours(:,3)';
+    
+    textGroup = hggroup;
+    % draw some text
+    textHandles = arrayfun(@makeATextHandle, words, wordCount, ...
+        r, g, b, sum(corrMat), 'UniformOutput', false);
+    % add all text to text group
+    cellfun(@(th) set(th, 'Parent', textGroup), textHandles);
+    % adding things into a group seems to flip the ordering, so...
+    textGroup.Children = flip(textGroup.Children);
+end
+
+function jLength = calculateJourneyLength(journey, corrMat)
+       x1 = journey(1:(end-1));
+       x2 = journey(2:end);
+       
+       % this neat line makes a vector [A(x1(1), x2(1)); A(x1(2), x2(2)); 
+       % A(x1(3), x2(3)); A(x1(4), x2(4));  etc ]
+       distanceBetweenOrderedWords = corrMat(sub2ind(size(corrMat), x1, x2));
+       jLength = cumsum([0 distanceBetweenOrderedWords]);
+end
+
+function colours = generateWordRGBColours(wordOrder, corrMat)
+    % will make an Nx3 matrix, where N is the number of words.
+    % step through the words in the best order, to find the cumulative distance
+    % travelled and the total distance travelled.
+    
+    % rescale correlation matrix to 0 for most correlation to 1 for least.
+    extremifyColouringFactor = 2; % makes colour difference more noticable when there is a big step between words.
+    rescaledCorrMat = (-0.5*corrMat+0.5).^extremifyColouringFactor;
+    
+    cumulativeWordDistances = calculateJourneyLength(wordOrder, rescaledCorrMat);
+    totalWordDistance = cumulativeWordDistances(end);
+    
+    % use cumulative word distances to scale the word colouring
+    mapIndex = round(cumulativeWordDistances ./ (totalWordDistance / 255))+1;
+    colmap = parula(256);
+    colours = colmap(mapIndex, :);
+end
+
+
+function th = makeATextHandle(word, wordCount, r, g, b, totalCorrelation)
+    fontScaleFactor = log10(wordCount);
+    fontBlockSize = 0.002; % measured in "FontUnits"
+    makeBackground = false;
+    prettyFonts = {'Century Gothic', 'Cooper Black', 'Magneto Bold'};   
+    rgb = [r,g,b]; % array fun does not like to pass vectors.
+    
+    th = text('String', word, ...
+        'FontName', prettyFonts{randi(numel(prettyFonts), 1)}, ... % random pretty font
+        'Color', rgb, ...
+        'Margin', 1, ...
+        'FontUnits', 'normalized', ...
+        'FontSize', (wordCount/fontScaleFactor)*fontBlockSize, ...
+        'Units', 'normalized', ... % 'data', ... %
+        'VerticalAlignment', 'middle', ...
+        'HorizontalAlignment', 'center', ...
+        'Position', [0,0] ...
+        );
+    if makeBackground
+        set(th, 'BackgroundColor', 1-rgb);
+    end
+    % find out how many font blocks tall and wide the text is
+    userData.blocksWide = ceil(th.Extent(3)/fontBlockSize);
+    userData.blocksHigh = ceil(th.Extent(4)/fontBlockSize);
+    userData.wordCount  = wordCount;
+    userData.tCorr      = totalCorrelation;
+    set(th, 'UserData', userData);
+end
+
+
+
+
+
