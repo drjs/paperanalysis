13293839f52c0029cc99354b67cfc5860354073e
U-MATHWORKS\owenj <owenj@UK-OWENJ.dhcp.mathworks.com>
2014-08-27 17:14:12 +0100

    started a new class for generating clouds of words. Previously only had clusters so there could be only 1 cluster of words. A cloud is a collection of these clusters, nicely spaced out.

---
+++ visualisation/WordCloud.m
@@ -0,0 +1,17 @@
+classdef WordCloud
+    
+    properties
+        clusters;
+    end
+    
+    methods
+        function this = WordCloud(wordList, wordCounts, wordCorrelations, clusterGroups)
+        end
+    end
+    
+    methods (Access = private)
+       % function tf = isClusterOverlap(cluster1, cluster2)
+    end
+    
+end
+
