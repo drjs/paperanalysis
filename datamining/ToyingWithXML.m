e51b78953a4eda9a29cc64dea5627566cbb3a17a
owenj <owenj@UK-OWENJ.dhcp.mathworks.com>
2014-09-08 16:29:44 +0100

    broken Pantelis code into separate functions for easier integration with word clouds.

--- datamining/ToyingWithXML.m
+++ datamining/ToyingWithXML.m
@@ -4,185 +4,19 @@
 clear
 close all
 
-
-A = xmlread('AbstractsEditted5.xml');
-allListitems = A.getElementsByTagName('p');
-raw_data = {};
-for indx = 1:allListitems.getLength
-    if ( ~isempty( allListitems.item(indx)))
-        Element = allListitems.item(indx).getFirstChild;
-        if (~isempty(Element))
-            if strcmpi(Element.getClass ,'class org.apache.xerces.dom.DeferredTextImpl')
-                raw_data{indx} = char(Element.getData);
-            end
-            if strcmpi(Element.getClass ,'class org.apache.xerces.dom.DeferredElementImpl')
-                if (~isempty(Element.getFirstChild))
-                    if strcmpi(Element.getFirstChild.getClass ,'class org.apache.xerces.dom.DeferredElementImpl')
-                        raw_data{indx} = 'NaN';
-                    else
-                        raw_data{indx} = char(Element.getFirstChild.getData);
-                    end
-                else
-                    raw_data{indx} = 'NaN';
-                end
-            end
-        else
-            raw_data{indx} = 'NaN';
-        end
-    else
-        raw_data{indx} = 'NaN';
-    end
-end
-
-clear Element indx allListitems A
-
-%% Make the data slightly nicer
-
-strip_newline_char  =@(s) strrep(s, char(10),' ');
-data_NoNewLineChars = cellfun(strip_newline_char, raw_data, 'UniformOutput', 0);
-clear raw_data strip_newline_char
-
-strip_double_spaces =@(s) regexprep(s,'\s{2,}', ' ');
-data_NoDoubleSpaces = cellfun(strip_double_spaces, data_NoNewLineChars, 'UniformOutput', 0);
-clear data_NoNewLineChars strip_double_spaces
-
-% Delete small words
-L = cellfun(@length, data_NoDoubleSpaces);
-data_NoDoubleSpaces(L <= 3) = [];
-clear L
-
-% Delete numerical only entries
-number_of_numbers = cellfun(@sum, isstrprop(data_NoDoubleSpaces ,'digit'));
-L = cellfun(@length, data_NoDoubleSpaces);
-data_NoDoubleSpaces( logical(round(number_of_numbers./L))) = [];
-clear number_of_numbers L
-
-data = data_NoDoubleSpaces';
-clear data_NoDoubleSpaces;
-
-%% Construct dataset indexing
-
-IndxPapAccpt = find(strcmp(data,'Status: Accepted'));
-IndxPapTitle = IndxPapAccpt-1;
-IndxCategory = find(~cellfun(@isempty, strfind(data ,'Category: ')));
-IndxAuthPref = find(~cellfun(@isempty, strfind(data ,'Author''s preference:')));
-IndxAuthName = IndxAuthPref +1;
-
-%PRoBable Abstract Indexing
-IndxProbAbst = setdiff(1:numel(data), ...
-    [IndxPapAccpt IndxPapTitle IndxCategory IndxAuthPref IndxAuthName])';
-
-
-%% Make abstracts
-
-% Probable Abstracts data entries
-ProbAbstracts = data(IndxProbAbst);
-
-% Replace small sentences
-L = cellfun(@length, ProbAbstracts);
-ProbAbstracts(L <= 20) = {' '};
-clear L
-
-% Make lowercase, strip puncation
-ProbAbstracts = lower(ProbAbstracts);
-ProbAbstracts = strrep(ProbAbstracts,':','');
-ProbAbstracts = strrep(ProbAbstracts,',','');
-ProbAbstracts = strrep(ProbAbstracts,'.','');
-ProbAbstracts = strrep(ProbAbstracts,'(','');
-ProbAbstracts = strrep(ProbAbstracts,')','');
-ProbAbstracts = strrep(ProbAbstracts,'[','');
-ProbAbstracts = strrep(ProbAbstracts,']','');
-ProbAbstracts = strrep(ProbAbstracts,';','');
-ProbAbstracts = strrep(ProbAbstracts,'\"','');
-ProbAbstracts = strrep(ProbAbstracts,'"','');
-ProbAbstracts = strrep(ProbAbstracts,'?','');
-ProbAbstracts = strrep(ProbAbstracts,'!','');
-ProbAbstracts = strrep(ProbAbstracts,'-',' ');
-ProbAbstracts = strrep(ProbAbstracts,char(39),'');
-
-% Concatenate relevant data entries
-
-Abstracts = {};
-for i= 1:numel(IndxPapTitle)-1
-    ithAbs = (find ((IndxPapTitle(i) < IndxProbAbst) & (IndxPapTitle(i+1) > IndxProbAbst )));
-    Abstracts(i) = {strjoin(ProbAbstracts(ithAbs)')};
-end
-ithAbs = (find ((IndxPapTitle(133) < IndxProbAbst) & (IndxPapTitle(133)+10 > IndxProbAbst )));
-Abstracts(133) = {strjoin(ProbAbstracts(ithAbs)')};
-
-clear ithAbs i   ProbAbstracts
-
-Indx.IndxAuthName =IndxAuthName;
-Indx.IndxAuthPref =IndxAuthPref;
-Indx.IndxCategory =IndxCategory;
-Indx.IndxPapAccpt =IndxPapAccpt;
-Indx.IndxPapTitle =IndxPapTitle;
-Indx.IndxProbAbst =IndxProbAbst;
-
-clear IndxAuthName IndxAuthPref IndxCategory ... 
-      IndxPapAccpt IndxPapTitle IndxProbAbst
+[wholeBody, titles, abstracts] = readSEFIXML('AbstractsEditted5.xml');
 
 %% Build the dictionary
 
-WholeBody = strsplit(strjoin(Abstracts));
+[uniqueKeywords, keywordFrequency] = extractKeywordsAndKeywordCount(wholeBody);
 
-% Delete small words
-L = cellfun(@length, WholeBody);
-WholeBody(L <= 3) = [];
+%% Make frequency count variants for each document for top N keywords
 
-% Make lower case
-WholeBody = lower(WholeBody);
+nwords = 30;
+topNWords = uniqueKeywords(1:nwords);
 
-% Construct the dictionary
-vocabulary = unique(WholeBody);
+[DocFreqL2, correlationMatrix] = extractKeywordCountAndCorrelationByDocument(topNWords, abstracts);
 
-fileid = fopen('CommonWords100oxforddictionaries.com.txt');
-Wcommon = textscan(fileid, '%s');
-if fileid ~=-1
-    fclose(fileid);
-end
-Wcommon = Wcommon{1};
-
-vocabulary(ismember(vocabulary,Wcommon)) = [];
-
-n = length(vocabulary);
-freq = zeros(n,1);
-for i=1:n
-    freq(i) = nnz(ismember(WholeBody,vocabulary{i}));
-end
-
-[freq, idx] = sort(freq, 'descend');
-vocabulary = vocabulary(idx);
-T = table(vocabulary',freq,'VariableNames',{'Vocabulary' 'Freq'});
-
-clear idx fileid Wcommon WholeBody freq i ans n L2 L
-
-
-%% Make frequency count variants for each document/term
- 
-Top30words = vocabulary(1:30);
-WordCounts = zeros(30,numel(Indx.IndxPapTitle));
-
-% Simple counting Words / Documents
-for i=1:30
-    for j = 1:numel(Indx.IndxPapTitle)        
-        WordCounts(i,j) = nnz(ismember( strsplit(Abstracts{j}) ,Top30words{i}));
-    end
-end
-
-Titles = data(Indx.IndxPapTitle);
-
-% TF-IDF normalization (Term Frequency. * Inverse Docum. Frequency)
-FreqCounts = bsxfun(@rdivide,WordCounts,sum(WordCounts));
-TF = log( FreqCounts + 1);
-IDF = log( 133 ./ (  sum( ~(WordCounts' <2*eps) ) + 1) );
-WordFreq_tfidf = TF .* repmat(IDF',1, 133);
-
-% L2 Normalization
-DocFreqNorms = cellfun(@norm, num2cell(WordFreq_tfidf,1));
-DocFreqL2 =  WordFreq_tfidf./repmat(DocFreqNorms,[30 1]);
-
-clear i j WordFreqNorms DocFreqNorms TF IDF
 
 %% Try some statistics and do naive visualizations
 
