9e96ad8456c8ba462963de3ee657b84a513ddcfe
Dr. Joachim Schlosser <jschloss@muc-jschloss1.dhcp.mathworks.com>
2015-06-05 09:34:37 +0200

    Limit to doc, docx and pdf make folder a variable for easier change save docParser structure in mat file for later usage.

--- StartAnalysis.m
+++ StartAnalysis.m
@@ -1,12 +1,16 @@
 function StartAnalysis()
 
-testfiles = dir('\\mathworks\marketing\Education\SharePoint_Big\ConferenceProceedings\SEFI2014\papers');
+%thedir = '\\mathworks\marketing\Education\SharePoint_Big\ConferenceProceedings\SEFI2014\papers';
+thedir = 'h:\Documents\MATLAB\paperanalysis\SEFI_2014';
+testfiles = dir(strcat(thedir,'\','*.doc*'));
+testfiles = [testfiles; dir(strcat(thedir,'\','*.pdf'))];
 testfiles = {testfiles.name};
 testfiles = {testfiles{3:end}}';
-testfiles = strcat('\\mathworks\marketing\Education\SharePoint_Big\ConferenceProceedings\SEFI2014\papers\', testfiles);
+testfiles = strcat(thedir, '\', testfiles);
 
 docParser = ParseFiles(testfiles, 'SEFI_2014');
 docParser.parse();
+save docParser;
 WordCloudEditor('parser', docParser);
 
 % testfiles = dir('testdata');
