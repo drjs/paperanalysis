e8bd35005c643ba35670e4d99a21630d838ca22a
Jenny <jenny@superhans>
2015-06-08 21:34:09 +0100

    finished save cloud to image button properly

--- StartAnalysis.m
+++ StartAnalysis.m
@@ -12,17 +12,7 @@ docParser = ParseFiles(testfiles, 'SEFI_2014');
 docParser.parse();
 save docParser;
 WordCloudEditor('parser', docParser);
-
-% testfiles = dir('testdata');
-% testfiles = {testfiles.name};
-% testfiles = {testfiles{3:end}}';
-% testfiles = cellfun(@(x) fullfile(pwd, 'testdata', x), testfiles, 'UniformOutput', false);
-% 
-% docparser = ParseFiles(testfiles, 'TestProject');
-% docparser.run();
-% rmdir(fullfile(pwd, 'TestProject'), 's');
-% generateWordCloud(docparser, 100);
-% generateSemanticSurface(docparser, 100);
+% generateSemanticSurface(docParser, 100);
     
 end
 
